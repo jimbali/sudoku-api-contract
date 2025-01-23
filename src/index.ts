53482ee53f41e2b658c0d84b19169b257c71392d
jimbali <magicode@outlook.com>
2025-01-23 21:31:01 +0000

    chore: Export UserSchema

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { apiContract } from "./contracts";
 import { puzzlesContract } from "./contracts";
 import { TallySchema } from "./zod";
 import { PuzzleSchema } from "./zod";
+import { UserSchema } from "./zod/user.schema";
 
 export {
   apiContract,
   puzzlesContract,
   PuzzleSchema,
   TallySchema,
+  UserSchema,
 }
